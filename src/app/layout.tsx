94e73221ddfc27439b63f5eb6f38de41a2ca3430
oyako99jp <oyako99@gmail.com>
2026-04-01 17:42:52 +0700

    feat: GTMをlayout.tsxに追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WQS6STS8');`}} />
         <link rel="icon" href="/favicon.ico" />
         <meta name="google-adsense-account" content="ca-pub-3938476116806210" />
         <script
@@ -42,7 +44,13 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WQS6STS8" height="0" width="0" style={{display:'none',visibility:'hidden'}} />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
